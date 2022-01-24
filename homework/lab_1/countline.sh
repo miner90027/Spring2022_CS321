c78f8d2d0da69acdf3fb746f9acecf59afdb3f64
miner90027 <alekszander.mccormick@outlook.com>
2022-01-24 11:12:52 -0900

    Added header comments

---
+++ homework/lab_1/countline.sh
@@ -0,0 +1,11 @@
+#
+# Aleks McCormick
+# Spring 2022 CS 321
+# 2022/24/01
+# countline.sh
+# shell that takes a file or directory and returns the total number of lines in the file
+#	if input was a directory, then reports total number of lines of all the files in all sub 
+#	directories
+#
+
+
