eca6b11ac61389e43a1ebbcc5bfbc086e4487c89
miner90027 <alekszander.mccormick@outlook.com>
2022-01-26 19:00:59 -0900

    Wrote shell to take user input file still working on counting the number of lines int the input file and storing it  in a variable

--- homework/lab_1/countline.sh
+++ homework/lab_1/countline.sh
@@ -4,9 +4,28 @@
 # 2022/24/01
 # countline.sh
 # shell that takes a file or directory and returns the total number of lines in the file
-#	if input was a directory, then reports total number of lines of all the files in all sub 
+#	if input was a directory, then reports total number of lines of all the files in all sub
 #	directories
 #
 
+#echo "hello" >&2 #prints to std error
 
-echo "hello" >&2 #prints to std error
+
+# Receive user input file
+echo "Please enter a file name"
+read -p 'File name: ' file
+
+#echo "$file"
+
+#count=$(wc -l $file)
+
+# count the number of lines in the file
+#echo "$file" | wc -l | (read count; temp +"$count")
+#count=$(wc -l $(file))
+
+cat $file | while read; do
+	count=$((count + 1))
+done
+
+# print the count to the screen
+echo "$count"
