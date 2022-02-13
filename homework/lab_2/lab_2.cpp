790476194e7ebc6d38b1bff3a9e21f4e3a055787
miner90027 <alekszander.mccormick@outlook.com>
2022-02-12 20:35:55 -0900

    Changed child process to loop indefenantly while the parent process is alive. parent process loops indefenantly until it receives an interupt signal. Parent process searching for SIGUSR1 & SIGUSR2 signals which are handled by sig_handler Child proccess can now send a signal to the parent process.

--- homework/lab_2/lab_2.cpp
+++ homework/lab_2/lab_2.cpp
@@ -10,29 +10,53 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
+#include <sys/wait.h>
 
-void sigHandler(int);
+void sig_handler(int);
 
 int main (void){
-
-	for(int i = 0; i < 5; ++i){
-		if(fork() == 0){
-			printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
-//			if(signal(SIGUSR1, sigHandler) == SIG_ERR)
-//				printf("\n can't catch SIGUSR1\n");
-			while(1)
-				sleep(1);
-		}
+	int pid = fork();
+	int status;
+	
+	if(pid == 0)// Child Process
+	{
+		do{
+		printf("[son] pid %d from [parent] pid %d\n", getpid(), getppid());
+		// send signal to parent
+		kill(getppid(), SIGUSR1);
+		
+		//signal(SIGUSR1, sig_handler);
+		// check parent status
+		waitpid(getppid(), &status, 0);
+		//sleep(1);
+		}while(WIFEXITED(status));// loop while parent is alive.
+		exit(0);// exit when parent exits
 	}
+
+
+	while(1){ // Parent Process
+		//wait(NULL);
+		signal(SIGUSR1, sig_handler);
+		signal(SIGUSR2, sig_handler);
+		wait(NULL);
+	}
+	//kill(pid, SIGINT);
 }
 
 
-void sigHandler(int signum){
+void sig_handler(int signum){
 	if(signum == SIGUSR1){
-		printf("Received SIGUSR1\n");
+		printf("Received SIGUSR1: %d\n", getpid());
 	}
 
 	if(signum == SIGUSR2){
-		printf("Received SIGUSR2");
+		printf("Received SIGUSR2: %d\n", getpid());
 	}
+/*	
+	if(signum == SIGINT){
+		if(getpid() == 0)
+			exit(0);
+		else
+			kill(getpid(), SIGINT);
+	}*/
 }
