0c6d5b1594e97d95c8da9a5ac375aa578f98e70f
miner90027 <alekszander.mccormick@outlook.com>
2022-03-05 17:20:42 -0900

    Modified client1.cpp to use seperate threads for sending and receiving messages modified build.sh to accomidate for the use of threads

--- homework/lab_3/client1.cpp
+++ homework/lab_3/client1.cpp
@@ -10,6 +10,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
+#include <pthread.h>
 #include <iostream>
 #include <string>
 #define PORT 8080
@@ -18,14 +19,20 @@ using std::endl;
 using std::cout;
 using std::cin;
 using std::getline;
-   
+
+int sock = 0, valread;
+
+void* sendM(void *ptr);
+void* receiveM(void *ptr);
+
 int main(int argc, char const *argv[])
 {
-    int sock = 0, valread;
     struct sockaddr_in serv_addr;
-    std::string message;
+   // std::string message;
+
+    pthread_t send, receive;
     
-    char buffer[1024] = {0};
+    //char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -50,7 +57,20 @@ int main(int argc, char const *argv[])
 
 	std::cout << "Connected to chat server. Type EXIT to end the connection." << std::endl;
 	
-    do{ 
+    do{
+    	pthread_create(&send, NULL, sendM, NULL);
+		pthread_create(&receive, NULL, receiveM, NULL);
+
+		pthread_join(send, NULL);
+		pthread_join(receive, NULL);
+
+    }while(true);
+    return 0;
+}
+
+void* sendM(void *ptr){
+		std::string message;
+	
     	std::cin.clear();
     	std::cin.sync();
     	// Receive message from user
@@ -59,20 +79,22 @@ int main(int argc, char const *argv[])
 			message = "Client has left conversation.";
 
 			send(sock, message.c_str(), message.size(), 0);
-			break;
+			exit(0);
+			//break;
 		}
 
     	// send message to server
 	    send(sock , message.c_str() , message.size() , 0 );
+}
 
+void* receiveM(void *ptr){
+		char buffer[1024] = {0};
+	
 		//clear out buffer for next round
-		for(int i = 0; i< sizeof(buffer); i++){ buffer[i] = 0;}
+		//for(int i = 0; i< sizeof(buffer); i++){ buffer[i] = 0;}
 
 	    //receive message from server
 	    valread = read( sock , buffer, 1024);
 
     	printf("%s\n",buffer );
-
-    }while(true);
-    return 0;
 }
