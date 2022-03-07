0de10b8f121fef682505b99be59b7e59ae2cd2a9
EnigmaSwanAdams <77408474+EnigmaSwanAdams@users.noreply.github.com>
2022-03-07 05:19:12 -0900

    server using non blocking sockets and with signal handler (not perfect)
    The issue appears to be that whenever Ctrl C is pressed on the server terminal it makes the select function that something has happened on both the master socket and the client socket, so once it expects something it gets stuck since that something doesn't exist (it was faked out by the signal handler somehow). I tried using a global Boolean to keep track of whether Ctrl C had been pushed, and I tried making sd a non blocking socket (although I suspect the client code means it already is anyway) and none of that worked.

---
+++ homework/lab_3/servern.c
@@ -0,0 +1,272 @@
+//Handle multiple socket connections with select and fd_set on Linux
+#include <stdio.h>
+#include <signal.h>
+#include <string.h> //strlen
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h> //close
+#include <arpa/inet.h> //close
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
+#include <iostream>
+#include <fcntl.h>
+#include <errno.h>
+
+#define TRUE 1
+#define FALSE 0
+#define PORT 8080
+
+using std::cout;
+using std::endl;
+
+void sigintHandler(int sig_num);
+//bool triedToLeave = false;
+
+
+int main(int argc , char *argv[])
+{
+
+signal(SIGINT, sigintHandler);
+
+int opt = TRUE;
+int master_socket , addrlen , new_socket , client_socket[30] ,
+max_clients = 30 , activity, i , valread , sd;
+int max_sd;
+struct sockaddr_in address;
+
+//char buffer[1025]; //data buffer of 1K
+char buffer[1024] = {0};
+    char buffer2[1024] = {0};
+
+//set of socket descriptors
+fd_set readfds;
+
+//a message
+char *message = "ECHO Daemon v1.0 \r\n";
+    char *hello = "Hello from server";
+    char *noOtherClientMsg = "couldn't find another client to send to\n";
+
+//initialise all client_socket[] to 0 so not checked
+for (i = 0; i < max_clients; i++)
+{
+client_socket[i] = 0;
+}
+
+//create a master socket
+if( (master_socket = socket(AF_INET , SOCK_STREAM , 0)) == 0)
+{
+perror("socket failed");
+exit(EXIT_FAILURE);
+}
+
+//set master socket to allow multiple connections ,
+//this is just a good habit, it will work without this
+if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt,
+sizeof(opt)) < 0 )
+{
+perror("setsockopt");
+exit(EXIT_FAILURE);
+}
+
+// SET SOCKET TO NONBLOCKING
+int flags = fcntl(master_socket, F_GETFL, 0);
+fcntl(master_socket, F_SETFL, flags | O_NONBLOCK);
+
+
+//type of socket created
+address.sin_family = AF_INET;
+address.sin_addr.s_addr = INADDR_ANY;
+address.sin_port = htons( PORT );
+
+//bind the socket to localhost port 8080
+if (bind(master_socket, (struct sockaddr *)&address, sizeof(address))<0)
+{
+perror("bind failed");
+exit(EXIT_FAILURE);
+}
+
+
+printf("Listener on port %d \n", PORT);
+
+//try to specify maximum of 3 pending connections for the master socket
+if (listen(master_socket, 3) < 0)
+{
+perror("listen");
+exit(EXIT_FAILURE);
+}
+
+//accept the incoming connection
+addrlen = sizeof(address);
+puts("Waiting for connections ...");
+
+while(TRUE)
+{
+//clear the socket set
+FD_ZERO(&readfds);
+
+//add master socket to set
+FD_SET(master_socket, &readfds);
+max_sd = master_socket;
+
+//add child sockets to set
+for ( i = 0 ; i < max_clients ; i++)
+{
+//socket descriptor
+sd = client_socket[i];
+
+//if valid socket descriptor then add to read list
+if(sd > 0)
+FD_SET( sd , &readfds);
+
+//highest file descriptor number, need it for the select function
+if(sd > max_sd)
+max_sd = sd;
+}
+
+//wait for an activity on one of the sockets , timeout is NULL ,
+//so wait indefinitely
+activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);
+
+if ((activity < 0) && (errno!=EINTR))
+{
+printf("select error");
+}
+
+//If something happened on the master socket ,
+//then its an incoming connection
+if (FD_ISSET(master_socket, &readfds) /*&& !triedToLeave*/)
+{
+
+    if ((new_socket = accept(master_socket,
+    (struct sockaddr *)&address, (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        printf("couldn't accept\n");
+        exit(EXIT_FAILURE);
+    }
+
+//inform user of socket number - used in send and receive commands
+printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs
+(address.sin_port));
+
+// say what the server recieved
+//for(int i= 0; i<5; i++){
+   // valread = read( new_socket , buffer, 1024);
+  //  printf("what we got string: %s\n",buffer );
+    //send(new_socket , hello , strlen(hello) , 0 );
+    //printf("Hello message sent %i from server\n", i);
+//}
+
+
+//send new connection greeting message
+//if( send(new_socket, message, strlen(message), 0) != strlen(message) )
+//{
+// perror("send");
+//}
+
+//puts("Welcome message sent successfully");
+
+//add new socket to array of sockets
+for (i = 0; i < max_clients; i++)
+{
+//if position is empty
+if( client_socket[i] == 0 )
+{
+client_socket[i] = new_socket;
+printf("Adding to list of sockets as %d\n" , i);
+
+break;
+}
+}
+}
+
+//else its some IO operation on some other socket
+//triedToLeave = false;
+for (i = 0; i < max_clients; i++)
+{
+            //printf("some other IO operation detected"); prints 30 times as expected
+sd = client_socket[i];
+           
+//while(true){
+if (FD_ISSET( sd , &readfds))
+{
+//Check if it was for closing , and also read the
+//incoming message
+                printf("if entered\n");
+
+                // clearout old buffer
+                for(int i = 0; i<max_clients; i++){
+                    //cout << "OB" << i << ":" << buffer[i] << endl;
+                    buffer[i] = 0;
+                }
+
+
+//Client has left conversation.
+// quits if even one client leaves (enters "EXIT")
+valread = read( sd , buffer, 1024);
+//bool clientHere = true;
+//if(buffer == "Client has left conversation."){ clientHere = false;}
+
+//if(!clientHere){printf("somebody exited\n");} // check if the above line works
+
+
+if (valread == 0 /* | !clientHere*/)
+{
+//Somebody disconnected , get his details and print
+getpeername(sd , (struct sockaddr*)&address , \
+(socklen_t*)&addrlen);
+printf("Host disconnected , ip %s , port %d \n" ,
+inet_ntoa(address.sin_addr) , ntohs(address.sin_port));
+
+//Close the socket and mark as 0 in list for reuse
+close( sd );
+client_socket[i] = 0;
+exit(0);
+}
+
+           
+//send the message that came in
+else
+{
+                    printf("it was a message\n"); // don't get here for some reason??
+//set the string terminating NULL byte on the end
+//of the data read
+buffer[valread] = '\0';
+                   
+                    // also send to orignial client (echo back)
+                    //send(client_socket[i], buffer, strlen(buffer), 0);
+                    //printf("message sent back to sender\n");
+
+                    // pick another client to send it to (a neigbor you don't get to pick who)
+                    if(client_socket[i-1] != 0){
+send(client_socket[i-1], buffer , strlen(buffer) , 0 );
+                        printf("message sent to previous client\n");
+                    }
+                    else if(client_socket[i+1] != 0){
+                    send(client_socket[i+1], buffer, strlen(buffer), 0);
+                    printf("message sent to next client\n");
+                    }
+                    else{
+                    send(sd, noOtherClientMsg , strlen(noOtherClientMsg), 0);
+                    printf("server couldn't find another client to send to\n");
+                    }
+                 
+}
+}
+}
+}
+
+return 0;
+}
+
+void sigintHandler(int sig_num){
+signal(SIGINT, sigintHandler);
+//printf("cannot be terminated with Ctrl C\n");
+//triedToLeave = true;
+//exit(0);
+return;
+}
+
+   
