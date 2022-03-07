64ed2fd94e0ae8dedf15c08d463cb93d62ffc5c0
miner90027 <alekszander.mccormick@outlook.com>
2022-03-06 19:56:21 -0900

    Changed gears from trying to use threads and processes to using non-blocking sockets Idea gotten from RootCellar: https://github.com/RootCellar/CS321/tree/main/lab3 This change eliminates the issue of having too manny processes crashing the host device
    still need to update the server to acomidate for this change, Client program has been updated.

--- homework/lab_3/client1.cpp
+++ homework/lab_3/client1.cpp
@@ -7,13 +7,16 @@
  */
 #include <stdio.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
+//#include <sys/wait.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <string.h>
 #include <pthread.h>
 #include <iostream>
 #include <string>
+#include <fcntl.h>
+#include <errno.h>
+
 #define PORT 8080
 
 using std::endl;
@@ -21,19 +24,18 @@ using std::cout;
 using std::cin;
 using std::getline;
 
-int sock = 0, valread;
-
+/*
 void* sendM(void *ptr);
 void* receiveM(void *ptr);
-
+*/
 int main(int argc, char const *argv[])
 {
     struct sockaddr_in serv_addr;
-   // std::string message;
+	char buffer[1024] = {0};
+	int sock = 0, valread;
 
-    pthread_t send, receive;
+    //pthread_t send, receive;
     
-    //char buffer[1024] = {0};
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         printf("\n Socket creation error \n");
@@ -56,20 +58,73 @@ int main(int argc, char const *argv[])
         return -1;
     }
 
+    //set stdin to non-blocking - Idea from RootCellar
+    int flags = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, flags | O_NONBLOCK);
+
+    // Make the socket non-blocking
+    flags = fcntl(sock, F_GETFL, 0);
+    fcntl(sock, F_SETFL, flags | O_NONBLOCK);
+
 	std::cout << "Connected to chat server. Type 'Ctrl C' to end the connection." << std::endl;
-	
+
+	while(true){
+		//reset error
+		errno = 0;
+		//read from server
+		valread = read(sock, buffer, 1024);
+
+		if(errno == EAGAIN){
+			//No data... continue
+		}
+		else if(valread == 0){
+			cout << "----Disconnected----" << endl;
+			close(sock);
+			sock = -1;
+		}
+		else if(errno != 0){
+			perror("Read Error");
+			exit(EXIT_FAILURE);
+		}
+		else{
+			//print received data
+			buffer[valread] = 0;
+			cout << "Recieved: " << buffer << endl;
+		}
+
+		//receive user input & send to server
+		errno = 0;
+		valread = read(STDIN_FILENO, buffer, 1024);
+
+		if(errno == EAGAIN){
+
+		}
+		else{
+			buffer[valread] = 0;
+			send(sock, buffer, strlen(buffer), 0);
+		}
+	}
+
+}
+/*
     do{
+	//	pthread_create(&receive, NULL, receiveM, NULL);
 
     	pthread_create(&send, NULL, sendM, NULL);
+    	
     	if(fork() == 0){
+	//    	pthread_create(&send, NULL, sendM, NULL);
+	//		pthread_join(send, NULL);
+
 			pthread_create(&receive, NULL, receiveM, NULL);
 			pthread_join(receive, NULL);
+			
 		}
 
 		//wait(NULL);
 
 		pthread_join(send, NULL);
-		//pthread_join(receive, NULL);
+	//	pthread_join(receive, NULL);
 
     }while(true);
     return 0;
@@ -82,13 +137,14 @@ void* sendM(void *ptr){
     	std::cin.sync();
     	// Receive message from user
 	    getline(cin,message); // to get whole line including spaces
-		/*if(!message.compare("EXIT")){
-			//message = "Client has left conversation.";
-			cout << "You have left the conversation" << endl;
-			//send(sock, message.c_str(), message.size(), 0);
-			//exit(0);
-			//break;
-		}*/
+	    
+		if(!message.compare("BYE")){
+			//message = "Other user has left conversation.";
+			cout << "----You have left the conversation----" << endl;
+			send(sock, message.c_str(), message.size(), 0);
+			kill(getppid(), SIGINT);
+			return ptr;
+		}
 
     	// send message to server
 	    send(sock , message.c_str() , message.size() , 0 );
@@ -96,12 +152,17 @@ void* sendM(void *ptr){
 
 void* receiveM(void *ptr){
 		char buffer[1024] = {0};
-	
-		//clear out buffer for next round
-		//for(int i = 0; i< sizeof(buffer); i++){ buffer[i] = 0;}
 
 	    //receive message from server
 	    valread = read( sock , buffer, 1024);
 
     	cout << "Received: " << buffer << endl;
+    /*	if(strcmp(buffer, "BYE")){
+			cout << "----You have left the conversation----" << endl;
+			send(sock, buffer, strlen(buffer), 0);
+
+			kill(getppid(), SIGINT);
+		}
+		
 }
+*/
