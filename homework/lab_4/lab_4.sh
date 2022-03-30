d65e7b8dc14f93f1847c4aa33a67cf216b3f5ddf
miner90027 <alekszander.mccormick@outlook.com>
2022-03-30 11:02:16 -0800

    Added guestions from lab documentation left space for answering questions

--- homework/lab_4/lab_4.sh
+++ homework/lab_4/lab_4.sh
@@ -1,5 +1,45 @@
-# Lab 4
+#!bin/bash
 
-# Questions
+# Lab_4.sh
+# Aleks McCormick
+# 2022/03/30
+# Spring2022 CS 321
+# Shell program demonstrating some system admin commands
 
-#Answer 
+# 1) Use the command 'chmod' to modify the permissions to files & directories for owner, group, & others.
+#	Create a shell script & give the current user full permission and everyone else read & execute permissions only.
+
+
+
+# 2) Create a FAT, NTFS, and EXT3 filesystem in your linux box. Mount it in three seperate sub-directories inside /mnt/.
+
+
+
+# 3) Use teh 'df' command to test it.
+
+
+
+# 4) Now create three different users called 'testuser1', 'testuser2', & 'testuser3'.
+
+
+
+# 5) Create a group called 'myfsusser'
+
+
+
+# 6) Add all three of the users created to the group.
+
+
+
+# 7) Set the permission of the directory created so that only members of the group can access it.
+
+
+
+# 8) Write a program that will check if the current user has write permission to the directory created.
+#	Print results.
+
+
+
+# 9) Now modiify the '~./bashrc' file so that the yser will automatically get the message right after login.
+
+
