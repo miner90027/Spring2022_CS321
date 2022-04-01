5f2fb8cc0c08761f8309850744c709e24ee4e6f1
miner90027 <alekszander.mccormick@outlook.com>
2022-04-01 10:57:48 -0800

    Answered questions 2 - 6

--- homework/lab_4/lab_4.sh
+++ homework/lab_4/lab_4.sh
@@ -13,33 +13,53 @@
 
 # 2) Create a FAT, NTFS, and EXT3 filesystem in your linux box. Mount it in three seperate sub-directories inside /mnt/.
 
+	# FAT
+	dd if=/dev/zero of=fat-image.dd bs=1M count=10
+	mkfs.fat fat-image.dd
+	mkdir /mnt/FAT_Dir
+	mount fat-image.dd /mnt/FAT_Dir
 
+	# NTFS
+	dd if=/dev/zero of=ntfs-image.dd bs=1M count=10
+	mkfs.ntfs ntfs-image.dd --force
+	mkdir /mnt/NTFS_Dir
+	mount ntfs-image.dd /mnt/NTFS_Dir
+
+	# EXT3
+	dd if=/dev/zero of=ext3-image.dd bs=1M count=10
+	mkfs.ext3 ext3-image.dd --force
+	mkdir /mnt/EXT3_Dir
+	mount ext3-image.dd /mnt/EXT3_Dir
 
 # 3) Use teh 'df' command to test it.
 
-
+	df -h
 
 # 4) Now create three different users called 'testuser1', 'testuser2', & 'testuser3'.
 
+	sudo adduser testuser1
+	sudo adduser testuser2
+	sudo adduser testuser3
 
+# 5) Create a group called 'myfsuser'
 
-# 5) Create a group called 'myfsusser'
-
-
+	sudo groupadd myfuser
 
 # 6) Add all three of the users created to the group.
 
+	sudo usermod -a -G myfuser testuser1
+	sudo usermod -a -G myfuser testuser2
+	sudo usermod -a -G myfuser testuser3
+
+# 7) Set the permission of the FAT filesystem directory created so that only members of the group can access it.
 
 
-# 7) Set the permission of the directory created so that only members of the group can access it.
 
-
-
-# 8) Write a program that will check if the current user has write permission to the directory created.
+# 8) Write a program that will check if the current user has write permission to the FAT filesystem directory.
 #	Print results.
 
 
 
-# 9) Now modiify the '~./bashrc' file so that the yser will automatically get the message right after login.
+# 9) Now modiify the '~./bashrc' file so that the user will automatically get the message right after login.
 
 
