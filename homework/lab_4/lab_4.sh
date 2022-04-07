2dd2dde1ae3361c0961a868f9975ef1b38442666
miner90027 <alekszander.mccormick@outlook.com>
2022-04-06 18:54:48 -0800

    Finished answering all of the questions

--- homework/lab_4/lab_4.sh
+++ homework/lab_4/lab_4.sh
@@ -9,7 +9,15 @@
 # 1) Use the command 'chmod' to modify the permissions to files & directories for owner, group, & others.
 #	Create a shell script & give the current user full permission and everyone else read & execute permissions only.
 
-
+	# Verify at least one parameter
+	if(($# < 1))
+	then 
+		echo "Please enter at least one file or directory to set permissions."
+	else
+		for file in "$@"
+			chmod 755 -R file
+		done
+	fi
 
 # 2) Create a FAT, NTFS, and EXT3 filesystem in your linux box. Mount it in three seperate sub-directories inside /mnt/.
 
@@ -31,9 +39,9 @@
 	mkdir /mnt/EXT3_Dir
 	mount ext3-image.dd /mnt/EXT3_Dir
 
-# 3) Use teh 'df' command to test it.
+# 3) Use the 'df' command to test it.
 
-	df -h
+	sudo df -h
 
 # 4) Now create three different users called 'testuser1', 'testuser2', & 'testuser3'.
 
@@ -53,13 +61,24 @@
 
 # 7) Set the permission of the FAT filesystem directory created so that only members of the group can access it.
 
-
+	sudo umount /mnt/FAT_Dir && sudo mount -o remount, gid=myfsuser, uid=testuser1 /mnt/FAT_Dir
 
 # 8) Write a program that will check if the current user has write permission to the FAT filesystem directory.
 #	Print results.
 
-
+	
+	test -w mnt/FAT_Dir | if [[ $? ]]
+	then 
+		echo "You have permission to edit the /mnt/FAT_Dir directory."
+	else
+		echo "You are denied permission to edit the /mnt/FAT_Dir directory."
+	fi
 
 # 9) Now modiify the '~./bashrc' file so that the user will automatically get the message right after login.
 
-
+	test -w mnt/FAT_Dir | if [[ $? ]]
+	then 
+		echo "You have permission to edit the /mnt/FAT_Dir directory."
+	else
+		echo "You are denied permission to edit the /mnt/FAT_Dir directory."
+	fi
