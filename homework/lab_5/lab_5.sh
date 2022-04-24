852bcf7e37caa0ce347519f1a894dca4048d114c
miner90027 <alekszander.mccormick@outlook.com>
2022-04-23 19:27:05 -0800

    Edited shell script after testing each command to make sure they worked still cannot get the conda to attach to shell works fine when run from within the container

--- homework/lab_5/lab_5.sh
+++ homework/lab_5/lab_5.sh
@@ -7,25 +7,44 @@
 # Docker build script to install Anaconda & Jupyter notebook
 
 # pull simple ubuntu Docker image
-sudo docker pull ubuntu
-docker run -it ubuntu
+docker pull ubuntu
 
-apt-get update && apt-get -y upgrade
+# creat the docker container
+docker create -it --name lab_5 ubuntu
 
-# download anaconda installer script inside docker image
-wget https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh -O ~/anaconda.sh
+# copy the anaconda installer script into the container
+docker cp Anaconda3-2021.11-Linux-x86_64.sh lab_5:/anaconda.sh
+
+# start and attach to the container
+docker start lab_5
+
+# sudo docker start -ai lab_5
+# verify everything up to date
+docker exec -u 0 lab_5 /bin/bash -c "apt-get update && apt-get -y upgrade"
 
 # change file permissions
-chmod 777 anaconda.sh
+docker exec -u 0 lab_5 /bin/bash -c "chmod 777 anaconda.sh"
 
 # Run the anaconda installer in silent mode
-bash ~/anaconda.sh -b -p -f $HOME/anaconda
+docker exec -u 0 lab_5 /bin/bash -c "./anaconda.sh -b -p"
+
+# append .bashrc as needed to run conda
+docker exec -u 0 lab_5 /bin/bash -c 'echo "export PATH=/root/anaconda3/bin:$PATH" >> ~/.bashrc'
+
+docker exec -it -u 0 lab_5 /bin/bash -c 'eval "$(/root/anaconda3/conda shell.bash hook)"'
+
+#docker restart lab_5
+
+#docker exec -u 0 lab_5 /root/anaconda3/bin/conda "init"
+docker exec -u 0 lab_5 /bin/bash -c "conda init"
+
+docker restart lab_5
 
 # create Conda environment
-conda create -n $HOME/anaconda
+docker exec -u 0 lab_5 /bin/bash -c "conda create -q --name note"
 
 # Activate conda environment
-conda activate $HOME/anaconda
+docker exec -u 0 lab_5 /bin/bash -c "codna activate note"
 
 # install jupyter
-conda install -c anaconda jupyter
+docker exec -u 0 lab_5 /bin/bash -c "conda install -c -y note jupyter"
